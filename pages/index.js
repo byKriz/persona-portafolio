4bf21336c335fe56391c374f2bc951a93716485f
bykriz <cristianesquivelh@hotmail.com>
2023-02-17 14:57:41 -0400

    agregando estilos y un icono

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -12,9 +13,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
-        
+
+      <main className=" bg-white px-10">
+        <section className="min-h-screen">
+          <nav className=" py-10 mb-12 flex justify-between">
+            <h1 className=" text-xl">developedbyed</h1>
+            <ul className=" flex items-center">
+              <li>
+                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </main>
     </>
-  )
+  );
 }
