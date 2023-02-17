3114cfd07529900a67f6e23a8d122bc5ac9af2f4
bykriz <cristianesquivelh@hotmail.com>
2023-02-17 16:59:57 -0400

    comenzado a escribir los servicios que ofrezco

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png"; //nota cambiar la imagen despues
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +20,7 @@ export default function Home() {
       <main className=" bg-white px-10">
         <section className="min-h-screen">
           <nav className=" py-10 mb-12 flex justify-between">
-            <h1 className=" text-xl">developedbyed</h1>
+            <h1 className=" text-xl font-burtons">developedbyed</h1>
             <ul className=" flex items-center">
               <li>
                 <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
@@ -25,13 +28,54 @@ export default function Home() {
               <li>
                 <a
                   href="#"
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                 >
                   Resume
                 </a>
               </li>
             </ul>
           </nav>
+          <div className=" text-center p-5">
+            <h2 className=" text-5xl py-2 text-teal-600 font-medium">
+              Cristian Esquivel
+            </h2>
+            <h3 className="text-2xl py-2">Frontend Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Soy
+              <span className="text-teal-500"> Cristian Esquivel, </span>
+              un desarrollador frontend con experiencia en HTML, CSS, JavaScript
+              y ReactJS. Aquí encontraras mi portafolio, en donde presento algunos de mis proyectos más
+              recientes. ¡Gracias por visitar!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-12 py-3 text-gray-600">
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className="mx-auto mb-6 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <Image src={deved} layout="fill" objectFit="cover" />
+            {/* lo voy a tener que cambiar mas adelante */}
+          </div>
+        </section>
+
+        {/* seccion 2 */}
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Servicios que ofrezco</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Soy un desarrollador
+              <span className="text-teal-500"> frontend </span>
+              que ofrece servicios de alta calidad para la creación de sitios
+              web y aplicaciones web. Mi experiencia incluye el diseño, la
+              codificación y la implementación de soluciones
+              <span className="text-teal-500"> innovadoras </span>
+              utilizando tecnologías como
+              <span className="text-teal-500">
+                {" "}
+                HTML, CSS, JavaScript y ReactJS.
+              </span>
+            </p>
+          </div>
         </section>
       </main>
     </>
